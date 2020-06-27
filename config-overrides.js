508cb2d3886cb48db33d09ca1bee5dc48c1a9856
Chuntong Gao <heihegao@gmail.com>
2020-06-27 11:11:31 -0400

    update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,26 +1,12 @@
-// integrate less into CRA: // https://www.jianshu.com/p/94ac7250ccf0
-
-// // eslint-disable-next-line @typescript-eslint/no-var-requires
-// const { override, addLessLoader } = require('customize-cra');
-
-// module.exports = override(
-//     addLessLoader({
-//         strictMath: true,
-//         noIeCompat: true,
-//         javascriptEnabled: true,
-//         cssLoaderOptions: {
-//             modules: { localIdentName: '[name]_[local]_[hash:base64:5]' }
-//         }
-//     })
-// );
+// integrate less into CRA:
+// https://www.jianshu.com/p/94ac7250ccf0
+// https://blog.bitsrc.io/using-less-with-create-react-app-without-ejecting-510a3344ef5d
 
 const rewireLess = require('react-app-rewire-less');
 
 module.exports = function override(config, env) {
-
     config = rewireLess.withLoaderOptions({
             javascriptEnabled: true,
     })(config, env);
-
     return config;
 };
